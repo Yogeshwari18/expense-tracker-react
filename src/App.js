3f9e893b6cfc9679b1a27ddcddcb59898388bc43
Yogeshwari18 <yogikumawat09@gmail.com>
2022-01-18 14:33:51 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
      <Header />
      <div className="container">
        <Balance />
@@ -19,7 +19,7 @@ function App() {
        <TransactionList />
        <AddTransaction />
      </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
